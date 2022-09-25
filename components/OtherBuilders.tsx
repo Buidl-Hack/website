693b0dae822b68546626bb52b068a3cdd406b502
Fidexmr <fidexmr@dnmx.org>
2022-09-25 09:12:28 +0800

    Add tooltip on pfp

--- components/OtherBuilders.tsx
+++ components/OtherBuilders.tsx
@@ -16,19 +16,16 @@ export default function OtherBuilders() {
       <h2>Most active hubsters</h2>
       <ul className={style.list}>
         {mostActive.map((member, index) => (
-          <li
-            className={style.member}
-            key={index}
-            data-tip={member}
-            data-for={member}
-            data-event="hover"
-          >
-            <Image
-              src={`/nfts/profile/${index + 1}.png`}
-              alt="nft"
-              width="175"
-              height="175"
-            />
+          <li className={style.member} key={index}>
+            <div className={style.image}>
+              <Image
+                src={`/nfts/profile/${(index % 19) + 1}.png`}
+                alt="nft"
+                width="175"
+                height="175"
+              />
+            </div>
+            <div className={style.name}>{member}</div>
           </li>
         ))}
       </ul>
